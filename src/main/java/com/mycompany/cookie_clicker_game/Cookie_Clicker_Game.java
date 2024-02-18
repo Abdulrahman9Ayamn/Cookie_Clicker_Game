a0ad35d1d0fb9bcfd3c99481ad1a81e9552bcfee
Abdulrhman <body1999221@gmail.com>
2024-02-18 03:57:11 +0200

    Createed the main Project Structure

---
+++ src/main/java/com/mycompany/cookie_clicker_game/Cookie_Clicker_Game.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cookie_clicker_game;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author qwertet
+ */
+public class Cookie_Clicker_Game {
+
+    public static void main(String[] args) throws IOException {
+       new CookioClicker().setVisible(true);
+    }
+}
